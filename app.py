209ab15629c9cfaf787e60196480ec95197c47c6
Franck <franck.somarribarodriguez@slu.edu>
2022-10-11 21:24:17 -0500

    MC-23 photos have been stored in the static folder, and html now shows the correct photo with the restaurant

--- app.py
+++ app.py
@@ -8,25 +8,12 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home')
 def home():
-    return render_template('index.html', food=randomizer.food(), foodlist=randomizer.fastFoodDict, )
-
-
-@app.route('/home')
-def contact():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'What you want?':
-            return "hello world"
-        elif request.form['submit_button'] == 'Do Something Else':
-            pass  # do something else
-        else:
-            pass  # unknown
-    elif request.method == 'GET':
-        return render_template('index.html', food=randomizer.food(), foodlist=randomizer.fastFoodDict)
+    return render_template('index.html', food=randomizer.food(), foodlist=randomizer.fastFoodDict)
 
 
 @app.route('/about')
 def about():
-    return "This is the about page"
+    return "This is the about page, here you can find information about the page"
 
 
 if __name__ == "__main__":
