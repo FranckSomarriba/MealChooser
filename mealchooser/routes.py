000b0aa0ada2d1c5d5b5049f36d2c4925cb6137d
DeepakKumarVemula <deepakkumar.vemula@slu.edu>
2022-11-11 14:13:16 -0600

    MC-79 Created HTML that is different for someone that has log in

--- mealchooser/routes.py
+++ mealchooser/routes.py
@@ -91,3 +91,17 @@ def reset_token(token):
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form = form)
 
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
